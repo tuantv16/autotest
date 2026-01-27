6f28def7549a9c0a59d85b0eb09917ec3a1288e5
TUAN Tran Van <tuan.tranvan@vti.com.vn>
2026-01-27 17:17:58 +0700

    feat(TY205): testcase init

--- tests/modules/TY205/WTY20501/wty20501-init.spec.ts
+++ tests/modules/TY205/WTY20501/wty20501-init.spec.ts
@@ -143,11 +143,195 @@ test.describe('WTY20501 - Summary Input (摘要欄入力)', () => {
 
         await summaryPage.navigate();
         await page.waitForTimeout(1000);
-
-       //verify text お届け予定日 tồn tại trong div body 
-       const isTextVisible = await summaryPage.isTextVisible(testData.formData.deliveryLabel);
-       expect(isTextVisible).toBe(true);
+        const isTextVisible = await summaryPage.isTextVisible(testData.formData.deliveryLabel);
+        expect(isTextVisible).toBe(true);
         await snapExpect();
     });
+
+    test('WTY10401_14', async ({
+        page,
+        baseUrl,
+        indexedDBHelper,
+        snapInput,
+        snapExpect,
+    }) => {
+        const testData = loadTestData('TY205/wty20501', 'wty20501', 'TC_03');
+        await page.goto(baseUrl, { waitUntil: 'domcontentloaded' });
+        await page.waitForTimeout(500);
+
+        await indexedDBHelper.initializeDB({
+            sessionData: testData.sessionData,
+            commonData: testData.commonData
+        });
+        
+        await page.waitForTimeout(500);
+
+        await summaryPage.navigate();
+        await page.waitForTimeout(1000);
+
+        const isTextVisible = await summaryPage.isTextVisible(testData.formData.guestNameKanjiLabel);
+        expect(isTextVisible).toBe(true);
+        await snapExpect();
+    });
+
+    test('WTY10401_15', async ({
+        page,
+        baseUrl,
+        indexedDBHelper,
+        snapInput,
+        snapExpect,
+    }) => {
+        const testData = loadTestData('TY205/wty20501', 'wty20501', 'TC_03');
+        await page.goto(baseUrl, { waitUntil: 'domcontentloaded' });
+        await page.waitForTimeout(500);
+
+        await indexedDBHelper.initializeDB({
+            sessionData: testData.sessionData,
+            commonData: testData.commonData
+        });
+        
+        await page.waitForTimeout(500);
+
+        await summaryPage.navigate();
+        await page.waitForTimeout(1000);
+
+        const isTextVisible = await summaryPage.isTextVisible(testData.formData.guestNameKanaLabel);
+        expect(isTextVisible).toBe(true);
+        await snapExpect();
+    });
+
+    test('WTY10401_16', async ({
+        page,
+        baseUrl,
+        indexedDBHelper,
+        snapInput,
+        snapExpect,
+    }) => {
+        const testData = loadTestData('TY205/wty20501', 'wty20501', 'TC_03');
+        await page.goto(baseUrl, { waitUntil: 'domcontentloaded' });
+        await page.waitForTimeout(500);
+
+        await indexedDBHelper.initializeDB({
+            sessionData: testData.sessionData,
+            commonData: testData.commonData
+        });
+        
+        await page.waitForTimeout(500);
+
+        await summaryPage.navigate();
+        await page.waitForTimeout(1000);
+
+        const isTextVisible = await summaryPage.isTextVisible(testData.formData.guestNameHonorificLabel);
+        expect(isTextVisible).toBe(true);
+        await snapExpect();
+    });
+
+    test('WTY10401_17', async ({
+        page,
+        baseUrl,
+        indexedDBHelper,
+        snapInput,
+        snapExpect,
+    }) => {
+        const testData = loadTestData('TY205/wty20501', 'wty20501', 'TC_03');
+        await page.goto(baseUrl, { waitUntil: 'domcontentloaded' });
+        await page.waitForTimeout(500);
+
+        await indexedDBHelper.initializeDB({
+            sessionData: testData.sessionData,
+            commonData: testData.commonData
+        });
+        
+        await page.waitForTimeout(500);
+
+        await summaryPage.navigate();
+        await page.waitForTimeout(1000);
+
+        const isTextVisible = await summaryPage.isTextVisible(testData.formData.paymentMethodLabel);
+        expect(isTextVisible).toBe(true);
+        await snapExpect();
+    });
+
+    test('WTY10401_18', async ({
+        page,
+        baseUrl,
+        indexedDBHelper,
+        snapInput,
+        snapExpect,
+    }) => {
+        const testData = loadTestData('TY205/wty20501', 'wty20501', 'TC_01');
+        await page.goto(baseUrl, { waitUntil: 'domcontentloaded' });
+        await page.waitForTimeout(500);
+
+        await indexedDBHelper.initializeDB({
+            sessionData: testData.sessionData,
+            commonData: testData.commonData
+        });
+        
+        await page.waitForTimeout(500);
+
+        await summaryPage.navigate();
+        await page.waitForTimeout(1000);
+
+        const isTextVisible = await summaryPage.isTextVisible(testData.formData.estimateDeliveryLabel);
+        expect(isTextVisible).toBe(true);
+        await snapExpect();
+    });
+
+    test('WTY10401_19', async ({
+        page,
+        baseUrl,
+        indexedDBHelper,
+        snapInput,
+        snapExpect,
+    }) => {
+        const testData = loadTestData('TY205/wty20501', 'wty20501', 'TC_03');
+        await page.goto(baseUrl, { waitUntil: 'domcontentloaded' });
+        await page.waitForTimeout(500);
+
+        await indexedDBHelper.initializeDB({
+            sessionData: testData.sessionData,
+            commonData: testData.commonData
+        });
+        
+        await page.waitForTimeout(500);
+
+        await summaryPage.navigate();
+        await page.waitForTimeout(1000);
+
+        const isTextVisible = await summaryPage.isTextVisible(testData.formData.summaryLabel);
+        expect(isTextVisible).toBe(true);
+        await snapExpect();
+    });
+
+    test('WTY10401_20', async ({
+        page,
+        baseUrl,
+        indexedDBHelper,
+        snapInput,
+        snapExpect,
+    }) => {
+        const testData = loadTestData('TY205/wty20501', 'wty20501', 'TC_03');
+        await page.goto(baseUrl, { waitUntil: 'domcontentloaded' });
+        await page.waitForTimeout(500);
+
+        await indexedDBHelper.initializeDB({
+            sessionData: testData.sessionData,
+            commonData: testData.commonData
+        });
+        
+        await page.waitForTimeout(500);
+        await summaryPage.navigate();
+        await page.waitForTimeout(1000);
+
+        const isTextVisibleCancel = await summaryPage.isTextVisible(testData.formData.cancelLabel);
+        expect(isTextVisibleCancel).toBe(true);
+
+        const isTextVisibleConfirm = await summaryPage.isTextVisible(testData.formData.confirmLabel);
+        expect(isTextVisibleConfirm).toBe(true);
+
+        await snapExpect();
+    });
+
 });
     
